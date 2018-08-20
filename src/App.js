570e7b8b03cc6d3cd151a8552e4512ec69738d9f
George Murray <murrayge@mnstate.edu>
2018-08-20 01:18:28 -0700

    Dynaminc Stying beginning

--- src/App.js
+++ src/App.js
@@ -9,14 +9,17 @@ class App extends Component {
   state = {
     persons: [
       {
+        id: 1,
         name: 'Max',
         age: 28
       },
       {
+        id: 2,
         name: 'Manu',
         age: 29
       },
       {
+        id: 3,
         name: 'Stephanie',
         age: 26
       }
@@ -28,49 +31,57 @@ class App extends Component {
 
 
 
-  switchNameHandler = (newName) => {
-    // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
-    this.setState({
-      persons: [
-        {
-          name: newName,
-          age: 28
-        },
-        {
-          name: 'Manu',
-          age: 29
-        },
-        {
-          name: 'Stephanie',
-          age: 27
-        }
-      ]
-    })
-  }
+  // switchNameHandler = (newName) => {
+  //   // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
+  //   this.setState({
+  //     persons: [
+  //       {
+  //         name: newName,
+  //         age: 28
+  //       },
+  //       {
+  //         name: 'Manu',
+  //         age: 29
+  //       },
+  //       {
+  //         name: 'Stephanie',
+  //         age: 27
+  //       }
+  //     ]
+  //   })
+  // }
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        {
-          name: 'Max',
-          age: 28
-        },
-        {
-          name: event.target.value,
-          age: 29
-        },
-        {
-          name: 'Stephanie',
-          age: 26
-        }
-      ]
-    })
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+
+    //const person = Object.assign({}, this.state.persons[personIndex]);
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({persons: persons})
   }
 
   inputChangeHandler = (event) => {
     this.setState({
       username: event.target.value
     })
+  }
+
+  deletePersonHandler = (personIndex) => {
+    // const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
   }
 
   togglePersonsHandler = () => {
@@ -80,7 +91,8 @@ class App extends Component {
   render() {
 
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
@@ -92,19 +104,17 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          <Person
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age} />
-          <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}
-            click={this.switchNameHandler.bind(this, 'Max!')}
-            changed={this.nameChangedHandler}>My Hobbies: Racing</Person>
-          <Person
-            name={this.state.persons[2].name}
-            age={this.state.persons[2].age} />
+          {this.state.persons.map((person, index) => {
+            return <Person 
+            click={() => this.deletePersonHandler(index)}
+            name={person.name}
+            age={person.age}
+            key={person.id} 
+            changed={(event) => this.nameChangedHandler(event, person.id)}/>
+          })}
         </div> 
       );
+      style.backgroundColor = 'red';
     }
 
     return (
