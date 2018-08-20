8af98c8204d16bd185c57b7adb24ed2c13b59925
George Murray <murrayge@mnstate.edu>
2018-08-19 23:58:40 -0700

    Init commit

---
+++ src/UserInput/UserInput.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const userinput = (props) => {
+    return (
+        <div className="UserInput">
+            <input type="text" onChange={props.changed}/>
+        </div>
+    )
+};
+
+export default userinput;
---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const useroutput = (props) => {
+    return (
+        <div className="UserOutput">
+            <p>{props.username}</p>
+            <p>Tummy!!!</p>
+        </div>
+    )
+};
+
+export default useroutput;
