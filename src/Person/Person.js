7fb5d3e5fde1de6e158c5e6e132482adb937f1c8
George Murray <murrayge@mnstate.edu>
2018-09-11 13:15:35 -0700

    imported radium for sudo selecting

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,6 +1,12 @@
 import React from 'react';
+// import Radium from 'radium';
 import './Person.css'
 const person = (props) => {
+    // const style = {
+    //     '@media (min-width: 500px)': {
+    //         width: '450px'
+    //     }
+    // };
     return (
         <div className="Person">
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
